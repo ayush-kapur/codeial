be0696a72ed08498467dbf5c0ccf46a926f2b7e6
unknown <ayush15kapur@gmail.com>
2020-10-27 03:57:50 +0530

    adding views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,12 @@
 module.exports.profile = function(req,res){
-    res.end('<h1>User Profile</h1>');
+    //res.end('<h1>User Profile</h1>');
+    return res.render('profile',{
+        title: "Profile"
+    });
 }
 module.exports.post = function(req,res){
-    res.end('<h1>Users Post</h1>');
+    //res.end('<h1>Users Post</h1>');
+    return res.render('Post',{
+        title: "Post"
+    });
 }
