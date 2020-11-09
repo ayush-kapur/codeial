04df61c42808807f4d4f0ebcaad452058beb9662
unknown <ayush15kapur@gmail.com>
2020-11-10 01:53:03 +0530

    Check Authentication on Creating Post

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+// to authenticate if the user is logged in to share a post
+const passport = require('passport');
 
 const postController = require('../controllers/posts_controller');
 
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication,postController.create);
 
 module.exports = router
