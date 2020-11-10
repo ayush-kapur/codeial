af82d674f16fbc3e25d92b402e0690cfb46bfb01
unknown <ayush15kapur@gmail.com>
2020-11-10 20:29:05 +0530

    nested population and displaying comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -19,7 +19,15 @@ module.exports.home = function(req,res){
     // })
 
     // we use populate command(available in mongoose) access the user database
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate:{
+            path: 'user'
+        }
+    })
+    .exec(function(err,posts){
         if(err){
             console.log('Error in fetching posts from db');
             return;
